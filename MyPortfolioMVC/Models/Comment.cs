1983b6639ff09d4dd4af006879989ceef88da2d6
lloydnathanielarciaga <lloydnathanielarciaga@gmail.com>
2026-09-19 14:44:55 +0800

    Added Comment Model

---
+++ MyPortfolioMVC/Models/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace MyPortfolioMVC.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int ProjectId { get; set; }
+        public string AuthorName { get; set; }
+        public string Content { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+    }
+}
