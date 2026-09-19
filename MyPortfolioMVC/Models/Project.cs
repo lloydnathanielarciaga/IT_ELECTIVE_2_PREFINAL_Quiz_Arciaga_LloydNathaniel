6ac0ba9459960c598b871ea434c59685613472a9
lloydnathanielarciaga <lloydnathanielarciaga@gmail.com>
2026-09-19 14:41:51 +0800

    Added Project Model

---
+++ MyPortfolioMVC/Models/Project.cs
@@ -0,0 +1,11 @@
+﻿namespace MyPortfolioMVC.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string GitHubLink { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
