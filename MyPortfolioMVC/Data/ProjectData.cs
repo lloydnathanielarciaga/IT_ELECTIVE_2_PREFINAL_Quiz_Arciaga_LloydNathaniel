addc706f775e4c9bd05bbef977d2136906c01466
lloydnathanielarciaga <lloydnathanielarciaga@gmail.com>
2026-09-19 15:16:41 +0800

    Added Project Data

---
+++ MyPortfolioMVC/Data/ProjectData.cs
@@ -0,0 +1,25 @@
+﻿using MyPortfolioMVC.Models;
+
+namespace MyPortfolioMVC.Data
+{
+    public class ProjectData
+    {
+        public static List<Project> Projects { get; set; } = new List<Project>();
+        public static List<Comment> Comments { get; set; } = new List<Comment>();
+
+        static ProjectData()
+        {
+            for (int i = 1; i <= 14; i++)
+            {
+                Projects.Add(new Project
+                {
+                    Id = i,
+                    Title = $"",
+                    Description = $"",
+                    GitHubLink = $"",
+                    ImageUrl = $""
+                });
+            }
+        }
+    }
+}
