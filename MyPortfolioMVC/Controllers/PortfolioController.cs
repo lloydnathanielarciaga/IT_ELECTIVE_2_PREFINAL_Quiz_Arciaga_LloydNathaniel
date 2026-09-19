f28c6eccc64125b2450f482de05aa3a685860f9b
lloydnathanielarciaga <lloydnathanielarciaga@gmail.com>
2026-09-19 14:53:09 +0800

    Added Portfolio Controller

---
+++ MyPortfolioMVC/Controllers/PortfolioController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioMVC.Data;
+
+namespace MyPortfolioMVC.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(ProjectData.Projects);
+        }
+    }
+}
