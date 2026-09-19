cecdd4c8eec9666104e658fbc83e84136c3b77fe
lloydnathanielarciaga <lloydnathanielarciaga@gmail.com>
2026-09-19 15:17:01 +0800

    Added Auth Controller

---
+++ MyPortfolioMVC/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using MyPortfolioMVC.Models;
+using System.Security.Claims;
+
+namespace MyPortfolioMVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(User user)
+        {
+            if (user.Username == "admin" && user.Password == "admin123")
+            {
+                var claims = new List<Claim> { new Claim(ClaimTypes.Name, user.Username) };
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var principal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                TempData["SuccessMessage"] = "Secure Admin Session Granted.";
+                return RedirectToAction("Index", "Portfolio");
+            }
+
+            ViewBag.ErrorMessage = "Access Denied. Invalid credentials.";
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Portfolio");
+        }
+    }
+}
